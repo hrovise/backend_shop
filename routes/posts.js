966dc1239cda3e0a64db67dfbf0d19cd6c57b117
hrovi <hrovise@gmail.com>
2025-11-19 14:26:01 +0100

    add migration postservice ts

--- routes/posts.js
+++ routes/posts.js
@@ -21,22 +21,7 @@ const router = express.Router();
 // }
 const ROLE_ADMIN = 'ADMIN';
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback)=> {
-//     const isValid = MIME_TYPE_MAP[file.mimetype];
-//     let error = new Error("Invalid mime type");
-//     if(isValid){
-//       error= null;
-//     }
 
-//     callback(error, "./images");
-//   },
-//   filename: (req, file, callback)=>{
-//     const name = file.originalname.toLowerCase().split(' ').join('-');
-//     const ext = MIME_TYPE_MAP[file.mimetype];
-//     callback(null, name +'-'+ Date.now()+'.'+ ext)
-//   }
-// });
 
 router.post('/category', Auth, (req, res, next) => {
 
